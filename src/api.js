6e856628769d2ca97209eb4e13c871fe62dddcd9
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2021-04-27 22:42:35 -0500

    openWeather Api ok

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
  
-let container = document.querySelector(".aside");
+let containerMain = document.querySelector(".container__main");
+const containerDetails = document.querySelector(".container__today--items");
 const item = document.createElement("detail-main");
 const itemSmall = document.createElement("detail-small");
 const itemBig = document.createElement("detail-big");
@@ -9,31 +10,29 @@ const itemBig = document.createElement("detail-big");
 var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 var f=new Date();
 
-    fetch("https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=Bogota")
+   let RenderCity = (city)=>{
+       
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2b9d5356c23b0e624f7e214ac8116221&lang=sp&units=metric`)
     .then(response=> response.json()) 
     .then(dataCity=>{ 
         console.log(dataCity)
-        /* item.setAttribute("ubication", dataCity[0].title ) */
-
-        fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong=50.068,-5.316`)
-            .then(response=> response.json()) 
-            .then(dataGps=>{ 
-
-                console.log(dataGps)
-                fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${dataGps[0].woeid}`)
-                .then(response=> response.json()) 
-                .then(dataGps=>{ 
-    
-                    console.log(dataGps)
-                })
-    
-    
+        containerMain.innerHTML= `<detail-main ubication="${dataCity.name}" date="${f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear()}" temp="${dataCity.main.temp}" type="${dataCity.weather[0].main}" > </detail-main>`
+        containerDetails.innerHTML=`
         
-         
-            })
+        
+        
+        <detail-big title="Max Temperatura" content="${dataCity.main.temp_max}" footer""></detail-big>
 
+           
+        <detail-big title="Min Temperatura" content="${dataCity.main.temp_min}" footer""></detail-big>
+        <detail-big title="Presion" content="${dataCity.main.pressure}" footer""></detail-big>
+
+        <detail-big title="Viento" content="${dataCity.wind.speed}" footer""></detail-big>
+        `
 
      })
+    }
+    RenderCity("Lima")
      
     
      
