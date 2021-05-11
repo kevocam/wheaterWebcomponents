e04e320d123a1ae826416d1284dc24aa0b220428
Albert Kevin Camargo <kevocam.seo@gmail.com>
2021-05-11 10:40:55 -0500

    fixs

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports ={
   },
     entry:'./src/index.js', 
     output: {
-        path:path.resolve(__dirname, 'dist'),
+        path:path.resolve(__dirname, ''),
         filename:'index.js', 
     },
     resolve:{
